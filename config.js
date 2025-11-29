9faa2d0e6d5afcead8a9877058ba21f4780e4ddf
Ayaan <iamayaanalee@gmail.com>
2025-11-30 01:37:32 +0500

    chore: add more comments in config js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
-    PREFIX: '!',
-    ACTIVITY_TEXT: 'Listening to music',
-    ACTIVITY_TYPE: 'Listening', // 'Playing', 'Streaming', 'Listening', 'Watching'
-    ACTIVITY_STATUS: 'DoNotDisturb'   // 'Online', 'Idle', 'DoNotDisturb', 'Invisible'
+    PREFIX: '!',                  // Prefix for prefix commands
+    ACTIVITY_TEXT: 'with Discord',// Bot presence text
+    ACTIVITY_TYPE: 'Playing',     // Playing, Watching, Listening, etc.
+    ACTIVITY_STATUS: 'Online',    // Online, Idle, DoNotDisturb, Invisible
 };
