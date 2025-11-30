138ada09e68acb01562638f33762f9033903b7fd
Ayaan <iamayaanalee@gmail.com>
2025-11-30 07:15:58 +0500

    chore: update activity type comment in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
     PREFIX: '!',                  // Prefix for prefix commands
     ACTIVITY_TEXT: 'with Discord',// Bot presence text
-    ACTIVITY_TYPE: 'Playing',     // Playing, Watching, Listening, etc.
+    ACTIVITY_TYPE: 'Playing',     // Playing, Watching, Listening, Competing.
     ACTIVITY_STATUS: 'Online',    // Online, Idle, DoNotDisturb, Invisible
 };
